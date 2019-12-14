e9d55cf4fe810cff25beed8368f7677f3b3a76ef
Paul Leugemors <pleugemors@yahoo.com>
2019-12-14 01:27:48 +0000

    Automatically backed up by Learn

---
+++ lib/chatty_student.rb
@@ -0,0 +1,8 @@
+class ChattyStudent < Student 
+  
+  def hello
+    super
+    puts "How are you doing today? I'm okey, but I'm Kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+  
+end
